d99a3bcfce9e73d0598051d74d8f86828720d2fc
Paulo Pereira <paulo_aa_pereira@outlook.pt>
2021-03-30 01:02:18 +0100

    # Added AutoCompleteBox tests

--- app/src/androidTest/java/com/example/androiddevchallenge/AutoCompleteBoxTest.kt
+++ app/src/androidTest/java/com/example/androiddevchallenge/AutoCompleteBoxTest.kt
@@ -15,25 +15,148 @@
  */
 package com.example.androiddevchallenge
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertHeightIsAtLeast
+import androidx.compose.ui.test.assertHeightIsEqualTo
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.domain.models.Person
 import com.example.androiddevchallenge.presentation.MainActivity
+import com.example.androiddevchallenge.presentation.components.autocomplete.AutoCompleteBoxTag
+import com.example.androiddevchallenge.presentation.sample.AutoCompleteSample
+import com.example.androiddevchallenge.presentation.sample.AutoCompleteSearchBarTag
+import com.example.androiddevchallenge.presentation.theme.MyTheme
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class AutoCompleteBoxTest {
+
     @get:Rule
     val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    @Test
-    fun sampleTest() {
-        // Add instrumented tests here
+    @ExperimentalAnimationApi
+    private fun launchAutoCompleteWith(persons: List<Person>) {
+        composeTestRule.setContent {
+            MyTheme {
+                AutoCompleteSample(persons = persons)
+            }
+        }
     }
+
+    @ExperimentalAnimationApi
+    @Test
+    fun auto_complete_box_not_visible_without_items() {
+        // Given
+        val persons = listOf<Person>()
+
+        // When
+        launchAutoCompleteWith(persons)
+
+        // Then
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performClick()
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).assertHeightIsEqualTo(0.dp)
+    }
+
+    @ExperimentalAnimationApi
+    @Test
+    fun auto_complete_box_visible_with_items() {
+        // Given
+        val persons = listOf(
+            Person(
+                name = "Paulo Pereira",
+                age = 23
+            )
+        )
+
+        // When
+        launchAutoCompleteWith(persons)
+
+        // Then
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performClick()
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).assertHeightIsAtLeast(1.dp)
+    }
+
+    @ExperimentalAnimationApi
+    @Test
+    fun auto_complete_box_visible_with_one_item() {
+        // Given
+        val persons = listOf(
+            Person(
+                name = "Paulo Pereira",
+                age = 23
+            )
+        )
+        val expectedChildrenCount = 1
+
+        // When
+        launchAutoCompleteWith(persons)
+
+        // Then
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performClick()
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).onChildren().assertCountEquals(expectedChildrenCount)
+    }
+
+    @ExperimentalAnimationApi
+    @Test
+    fun auto_complete_box_filter_has_one_result() {
+        // Given
+        val persons = listOf(
+            Person(
+                name = "Paulo Pereira",
+                age = 23
+            ),
+            Person(
+                name = "Daenerys Targaryen",
+                age = 24
+            ),
+            Person(
+                name = "Jon Snow",
+                age = 24
+            ),
+            Person(
+                name = "Sansa Stark",
+                age = 20
+            ),
+        )
+        val expectedChildrenCount = 1
+        val textInput = "Paul"
+
+        // When
+        launchAutoCompleteWith(persons)
+
+        // Then
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performClick()
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).assertHeightIsAtLeast(1.dp)
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performTextInput(textInput)
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).onChildren().assertCountEquals(expectedChildrenCount)
+    }
+
+    @ExperimentalAnimationApi
+    @Test
+    fun auto_complete_box_filter_empty_results() {
+        // Given
+        val persons = listOf(
+            Person(
+                name = "Paulo Pereira",
+                age = 23
+            )
+        )
+        val expectedChildrenCount = 0
+        val textInput = "random query"
+
+        // When
+        launchAutoCompleteWith(persons)
+
+        // Then
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performClick()
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).assertHeightIsAtLeast(1.dp)
+        composeTestRule.onNodeWithTag(AutoCompleteSearchBarTag).performTextInput(textInput)
+        composeTestRule.onNodeWithTag(AutoCompleteBoxTag).onChildren().assertCountEquals(expectedChildrenCount)
+    }
+
 }
